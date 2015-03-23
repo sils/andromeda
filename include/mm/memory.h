419e3bb29326ef308acef085255b54e69959f3ab
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2015-03-23 11:11:59 +0100

    mm: memcmp: Add difference direction detection
    Previously, the operation would only yield in case of a difference. This
    doesn't follow the C specification however, so now, the operation
    returns:
    
    -1: if smaller,
    0: if equal,
    1: if larger
    
    Signed-off-by: Bart Kuivenhoven <bemkuivenhoven@gmail.com>

--- include/mm/memory.h
+++ include/mm/memory.h
@@ -30,13 +30,13 @@ extern "C" {
 void paging();
 void memset(void*, int, size_t);
 void memcpy(void*, void*, size_t);
-int memcmp(void*, void*, size_t);
+int memcmp(const void*, const void*, size_t);
 #ifdef SLOB
 int init_heap();
 int complement_heap(void*, size_t);
 #endif
 
-size_t strlen(char* string);
+size_t strlen(const char* string);
 
 #ifdef __INTEL
 void setGDT();
--- src/mm/memory.c
+++ src/mm/memory.c
@@ -1,25 +1,26 @@
 /*
-    Andromeda
-    Copyright (C) 2011, 2012
-    Bart Kuivenhoven, Steven van der Schoot, Michel Megens
+ Andromeda
+ Copyright (C) 2011, 2012
+ Bart Kuivenhoven, Steven van der Schoot, Michel Megens
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdlib.h>
 #include <mm/paging.h>
 #include <mm/heap.h>
+#include <mm/memory.h>
 
 #ifndef SLAB
 #define BASE_HEAP_SIZE 0x400000
@@ -44,8 +45,7 @@ init_heap()
 #endif
 void memset(void *dest, int sval, size_t count)
 {
-        if (!count)
-        {
+        if (!count) {
                 return;
         }
         sval &= 0x000000ff;
@@ -71,25 +71,21 @@ void memset(void *dest, int sval, size_t count)
 #else
         unsigned int val = (unsigned int) sval;
         char i = 8;
-        for (; i < 32; i += 8)
-        {
+        for (; i < 32; i += 8) {
                 val |= (sval << i);
         }
-        while (count >= 4)
-        {
+        while (count >= 4) {
                 *(unsigned int*) dest = (unsigned int) val;
                 dest += 4;
                 count -= 4;
         }
 #endif
-        if (count >= 2)
-        {
+        if (count >= 2) {
                 *(unsigned short*) dest = (unsigned short) val;
                 dest += 2;
                 count -= 2;
         }
-        if (count >= 1)
-        {
+        if (count >= 1) {
                 *(unsigned char*) dest = (unsigned char) val;
         }
         return;
@@ -106,7 +102,7 @@ void memset(void *dest, int sval, size_t count)
 void memcpyBack(void *dest, void *src, size_t count)
 {
         dest += count;
-        src  += count;
+        src += count;
 #ifndef X86
 //64 bit int is only faster at 64-bit PC's, 32 bits prefers 2 time 32 int
         while (count >= 8)
@@ -124,23 +120,20 @@ void memcpyBack(void *dest, void *src, size_t count)
                 count -= 4;
         }
 #else
-        while (count >= 4)
-        {
+        while (count >= 4) {
                 *(unsigned int*) dest = *(unsigned int*) src;
                 dest -= 4;
                 src -= 4;
                 count -= 4;
         }
 #endif
-        if (count >= 2)
-        {
+        if (count >= 2) {
                 *(unsigned short*) dest = *(unsigned short*) src;
                 dest -= 2;
                 src -= 2;
                 count -= 2;
         }
-        if (count >= 1)
-        {
+        if (count >= 1) {
                 *(unsigned char*) dest = *(unsigned char*) src;
         }
         return;
@@ -148,8 +141,8 @@ void memcpyBack(void *dest, void *src, size_t count)
 
 void memcpy(void *dest, void *src, size_t count)
 {
-        if ((addr_t)src+count>(addr_t)dest)
-                memcpyBack(dest,src,count);
+        if ((addr_t) src + count > (addr_t) dest)
+                memcpyBack(dest, src, count);
 #ifndef X86
         //64 bit int is only faster at 64-bit PC's, 32 bits prefers 2 time 32 int
         while (count >= 8)
@@ -167,77 +160,46 @@ void memcpy(void *dest, void *src, size_t count)
                 count -= 4;
         }
 #else
-        while (count >= 4)
-        {
+        while (count >= 4) {
                 *(unsigned int*) dest = *(unsigned int*) src;
                 dest += 4;
                 src += 4;
                 count -= 4;
         }
 #endif
-        if (count >= 2)
-        {
+        if (count >= 2) {
                 *(unsigned short*) dest = *(unsigned short*) src;
                 dest += 2;
                 src += 2;
                 count -= 2;
         }
-        if (count >= 1)
-        {
+        if (count >= 1) {
                 *(unsigned char*) dest = *(unsigned char*) src;
         }
         return;
 }
 
-int memcmp(void *ptr1, void* ptr2, size_t count)
+int memcmp(const void* s1, const void *s2, size_t len)
 {
-#ifndef X86
-        //64 bit int is only faster at X86, X64 prefers 2 time 32 int
-        while (count >= 8)
-        {
-                if (*(unsigned long long*) ptr1 - *(unsigned long long*) ptr2)
+        size_t idx = 0;
+        const char* a = s1;
+        const char* b = s2;
+
+        for (; idx < len; idx++) {
+                if (a[idx] < b[idx]) {
+                        return -1;
+                } else if (a[idx] > b[idx]) {
                         return 1;
-                ptr1 += 8;
-                ptr2 += 8;
-                count -= 8;
-        }
-        if (count >= 4)
-        {
-                if (*(unsigned long long*) ptr1 - *(unsigned long long*) ptr2)
-                        return 1;
-                ptr1 += 4;
-                ptr2 += 4;
-                count -= 4;
-        }
-#else
-        while (count >= 4)
-        {
-                if (*(unsigned int*) ptr1 - *(unsigned int*) ptr2)
-                        return 1;
-                ptr1 += 4;
-                ptr2 += 4;
-                count -= 4;
-        }
-#endif
-        while(count >= 2)
-        {
-                if (*(unsigned short*) ptr1 - *(unsigned short*) ptr2)
-                        return 1;
-                ptr1 += 2;
-                ptr2 += 2;
-                count -= 2;
-        }
-        if ((count == 1)&&(*(unsigned char*) ptr1 - *(unsigned char*) ptr2))
-        {
-                return 1;
+                }
         }
         return 0;
 }
 
-size_t strlen(char* string)
+size_t strlen(const char* string)
 {
-        int i = 0;
-        for (; *(string + i) != '\0'; i++);
+        size_t i = 0;
+        for (; string[i] != '\0'; i++) {
+        }
         return i;
 }
 
